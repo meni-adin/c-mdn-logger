e38b6044947489bad120c509eeae310d69a7e812
Menachem Adin <meniadin@gmail.com>
2025-09-23 10:28:03 +0300

    Fix ignorance of includeDate in Win impl.

--- src/logger/logger.c
+++ src/logger/logger.c
@@ -173,7 +173,10 @@ static void mdn_Logger_printTimestamp(FILE *stream, bool includeDate) {
 
     GetLocalTime(&st);
 
-    fprintf(stream, "%04d-%02d-%02d %02d:%02d:%02d.%03d", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
+    if (includeDate) {
+        fprintf(stream, "%04d-%02d-%02d ", st.wYear, st.wMonth, st.wDay);
+    }
+    fprintf(stream, "%02d:%02d:%02d.%03d ", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
 #endif  // OS
 }
 
